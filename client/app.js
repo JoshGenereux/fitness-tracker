baa48f9201326f0f4a1e55b61d9ac6bb83d59d00
JoshGenereux <josh.genereux13@gmail.com>
2021-12-22 09:15:56 -0700

    adding modal

---
+++ client/app.js
@@ -0,0 +1,21 @@
+let modal = document.getElementById('create-modal')
+const createBtn = document.getElementById('custom-button')
+const closeBtn = document.getElementById('close')
+let customInput = document.getElementById('custom-input')
+
+createBtn.onclick = function (){
+   modal.style.display = "block";
+}
+function closeModal(e){
+   modal.style.display = "none";
+}
+window.onclick = function (e){
+   if(e.target === modal){
+      modal.style.display = "none";
+   }
+}
+function customInputHandler(){
+
+}
+customInput.addEventListener('submit', customInputHandler)
+closeBtn.addEventListener('click', closeModal)
