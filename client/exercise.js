bb7bb68ac83bc865b1fd1577e0116728ce253822
JoshGenereux <josh.genereux13@gmail.com>
2021-12-23 09:03:35 -0700

    added some html with js

--- client/exercise.js
+++ client/exercise.js
@@ -1,6 +1,7 @@
 const exerciseList = document.getElementById('exercise-list')
 const exerciseListBtn = document.getElementById('exercise-list-btn')
 const exerciseValue = document.getElementById('exercise-list')
+const addSet = document.getElementById('add-set')
 
 addWorkoutName()
 function addWorkoutName(){
@@ -19,10 +20,31 @@ window.onclick = function (event){
 }
 
 function addExercise(e){
-    const elementValue = e.target.id;
-    console.log(elementValue)
-    let exerciseSelector = document.queryCommandValue('')
-    console.log(exerciseSelector)
-    document.getElementById('exercise-name').innerHTML = elementValue
+    let setNum = 1;
+    document.getElementById('add-exercise-block').innerHTML =
+        '<div class="exercise">\n' +
+        '        <h2 id="exercise-name"></h2>\n' +
+        '        <div id="set">\n' +
+        '            <div id="set-set">set: <p id="set-count"></p></div>\n' +
+        '            <div id="set-weight">weight: <p id="set-weight-num"></p></div>\n' +
+        '            <div id="set-rep">reps: <p id="set-rep-num"></p></div>\n' +
+        '        </div>\n' +
+        '        <button id="add-set" onclick="addSetHandler()">Add Set</button>\n ' +
+        '</div>'
+    document.getElementById('exercise-name').innerHTML = e.target.id
+
 }
 exerciseValue.addEventListener('click',addExercise)
+
+function addSetHandler(){
+    document.getElementById('add-exercise-block').innerHTML =
+            '<div id="set">\n' +
+            ' <div id="set-set">set: <p id="set-count"></p></div>\n' +
+            ' <div id="set-weight">weight: <p id="set-weight-num"></p></div>\n' +
+            ' <div id="set-rep">reps: <p id="set-rep-num"></p></div>\n' +
+            ' </div>'
+}
+
+function addSetNum (){
+    const setDivOne = document.createElement('div').id = 'exercise'
+}
