f756a2e710d25eb7947dfb7df9b585148c5b03b0
JoshGenereux <josh.genereux13@gmail.com>
2021-12-24 13:48:40 -0700

    addexercise working correctly

--- client/exercise.js
+++ client/exercise.js
@@ -1,8 +1,7 @@
 const exerciseList = document.getElementById('exercise-list')
 const exerciseListBtn = document.getElementById('exercise-list-btn')
 const exerciseValue = document.getElementById('exercise-list')
-const addSet = document.getElementById('add-set-btn')
-const exercise = document.getElementById('add-exercise-block')
+exerciseValue.addEventListener('click', addExercise)
 
 addWorkoutName()
 function addWorkoutName(){
@@ -15,7 +14,6 @@ function toggleExerciseList(){
     } else {
         exerciseList.style.display = 'none'
     }
-
 }
 exerciseListBtn.addEventListener('click', toggleExerciseList)
 
@@ -26,16 +24,15 @@ window.onclick = function (event){
 }
 let setNum = 1;
 function addExercise(e){
-    document.getElementById('add-exercise-block').innerHTML =
+    document.getElementById('add-exercise-block').insertAdjacentHTML("afterend",
         '<div class="exercise">\n' +
         '        <h2 id="exercise-name"></h2>\n' +
         '        <div id="add-set"></div>\n' +
         '        <button id="add-set-btn" onclick="addSetHandler()">Add Set</button>\n ' +
-        '</div>'
+        '</div>')
     document.getElementById('exercise-name').innerHTML = e.target.id
-
+    toggleExerciseList()
 }
-exerciseValue.addEventListener('click', addExercise)
 
 function addSetHandler(){
     document.getElementById('add-set').insertAdjacentHTML("beforeend",
@@ -56,5 +53,35 @@ function addSetHandler(){
     setNum++;
 }
 
-
+// function addAnotherExercise (e){
+//     e.preventDefault()
+//     document.getElementById('add-exercise-block').insertAdjacentHTML("afterend",
+//         '<div class="exercise">\n' +
+//             '        <h2 id="another-name"></h2>\n' +
+//             '        <div id="add-another-set"></div>\n' +
+//             '        <button id="add-another-btn" onclick="addAnotherSet()">Add Set</button>\n ' +
+//             '</div>'
+//         )
+//     document.getElementById('another-name').innerHTML = e.target.id
+//     console.log('addAnotherExercise')
+// }
+//
+// function addAnotherSet(){
+//     document.getElementById('add-another-set').insertAdjacentHTML("beforeend",
+//         '<div id="add-set-set">\n' +
+//         '            <div id="set-set">\n' +
+//         '               <div>set:</div>\n' +
+//         '               <p id="set-set-count">'+setNum+'</p>\n'+
+//         '            </div>\n' +
+//         '            <div id="set-weight">\n' +
+//         '               <div>weight:</div>\n' +
+//         '               <input id="set-weight-num" type="number"/>\n'+
+//         '            </div>\n'+
+//         '            <div id="set-rep">\n' +
+//         '               <div>reps:</div>\n' +
+//         '               <input id="set-rep-num" type="number"/>\n'+
+//         '            </div>\n'+
+//         ' </div>')
+//     console.log('addAnotherSet')
+// }
 
